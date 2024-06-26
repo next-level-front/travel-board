f3411bcef6e1c16faed4704bafe92f8b7f82b154
kang taehyun <63766246+kwm2546@users.noreply.github.com>
2024-06-27 02:16:06 +0900

    feat

--- travel-board-app/src/index.js
+++ travel-board-app/src/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import DetailPost from "./components/DetailPost";
 import { MyProvider } from "./contexts/MyContext";
 
 ReactDOM.render(
   <React.StrictMode>
     <MyProvider>
-      <App />
+      <DetailPost />
     </MyProvider>
   </React.StrictMode>,
   document.getElementById("root")
