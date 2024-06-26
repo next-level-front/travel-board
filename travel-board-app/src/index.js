92a03305f69de07a7b8d00052217f05d9ec95cf1
seunghyo <tmdgy6232@naver.com>
2024-06-26 15:35:03 +0900

    update: App.js 구조 변경 및 index.js Context Provider 설정, UpdatePage 수정

--- travel-board-app/src/index.js
+++ travel-board-app/src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { MyProvider } from "./contexts/MyContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MyProvider>
+      <App />
+    </MyProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
